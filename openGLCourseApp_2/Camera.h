bb3c29d53d7a15bb8e1a7981e050a518314c7bea
toneskies <tony.g.badea@gmail.com>
2025-11-30 02:40:55 -0800

    add full undemy course, final

---
+++ openGLCourseApp_2/Camera.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <GL/glew.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <GLFW/glfw3.h>
+
+class Camera
+{
+
+public:
+	Camera();
+	Camera(glm::vec3 startPosition, glm::vec3 startUp, GLfloat startYaw, GLfloat startPitch, GLfloat startMoveSpeed, GLfloat startTurnSpeed);
+	
+	void keyControl(bool* keys, GLfloat deltaTime);
+	void mouseControl(GLfloat xChange, GLfloat yChange);
+
+	glm::vec3 getCameraPosition();
+	glm::vec3 getCameraDirection();
+	
+	glm::mat4 calculateViewMatrix();
+	
+	~Camera();
+
+private:
+	glm::vec3 position;
+	glm::vec3 front;
+	glm::vec3 up;
+	glm::vec3 right;
+	glm::vec3 worldUp;
+
+	glm::vec2 smoothedDelta;
+	GLfloat smoothingFactor;
+
+
+	GLfloat yaw;
+	GLfloat pitch;
+
+	GLfloat moveSpeed;
+	GLfloat turnSpeed;
+
+	void update();
+};
+
---
+++ openGLCourseApp_2/GLWindow.cpp
@@ -0,0 +1,167 @@
+#include "GLWindow.h"
+
+GLWindow::GLWindow()
+{
+	width = 800;
+	height = 600;
+
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = 0;
+	}
+
+	xChange = 0.0f;
+	yChange = 0.0f;
+
+	// Fix 1: Initialize these variables to prevent startup glitches
+	lastX = 0.0f;
+	lastY = 0.0f;
+	mouseFirstMoved = true;
+}
+
+GLWindow::GLWindow(GLint windowWidth, GLint windowHeight)
+{
+	width = windowWidth;
+	height = windowHeight;
+
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = 0;
+	}
+
+	xChange = 0.0f;
+	yChange = 0.0f;
+
+	// Fix 1: Initialize these variables here as well
+	lastX = 0.0f;
+	lastY = 0.0f;
+	mouseFirstMoved = true;
+}
+
+int GLWindow::Initialize()
+{
+	if (!glfwInit())
+	{
+		printf("Error Initialising GLFW");
+		glfwTerminate();
+		return 1;
+	}
+
+	// Setup GLFW Windows Properties
+	// OpenGL version
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Core Profile
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// Allow forward compatiblity
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	// Create the window
+	mainWindow = glfwCreateWindow(width, height, "Test Window", NULL, NULL);
+	if (!mainWindow)
+	{
+		printf("Error creating GLFW window!");
+		glfwTerminate();
+		return 1;
+	}
+
+	// Get buffer size information
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	// Set the current context
+	glfwMakeContextCurrent(mainWindow);
+
+	// Handle Key + Mouse Input
+	createCallbacks();
+	glfwSetInputMode(mainWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// Allow modern extension access
+	glewExperimental = GL_TRUE;
+
+	GLenum error = glewInit();
+	if (error != GLEW_OK)
+	{
+		printf("Error: %s", glewGetErrorString(error));
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+
+	// Create Viewport
+	glViewport(0, 0, bufferWidth, bufferHeight);
+
+	glfwSetWindowUserPointer(mainWindow, this);
+
+	// Explicitly check for successful initialization? Returns missing in original code, 
+	// but implied success is 0 or void. Since func is int, return 0;
+	return 0;
+}
+
+void GLWindow::createCallbacks()
+{
+	glfwSetKeyCallback(mainWindow, handleKeys);
+	glfwSetCursorPosCallback(mainWindow, handleMouse);
+}
+
+GLfloat GLWindow::getXChange()
+{
+	GLfloat theChange = xChange;
+	xChange = 0.0f;
+	return theChange;
+}
+
+GLfloat GLWindow::getYChange()
+{
+	GLfloat theChange = yChange;
+	yChange = 0.0f;
+	return theChange;
+}
+
+void GLWindow::handleKeys(GLFWwindow* window, int key, int code, int action, int mode)
+{
+	GLWindow* theWindow = static_cast<GLWindow*>(glfwGetWindowUserPointer(window));
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+		{
+			theWindow->keys[key] = true;
+		}
+		else if (action == GLFW_RELEASE)
+		{
+			theWindow->keys[key] = false;
+		}
+	}
+}
+
+void GLWindow::handleMouse(GLFWwindow* window, double xPos, double yPos)
+{
+	GLWindow* theWindow = static_cast<GLWindow*>(glfwGetWindowUserPointer(window));
+
+	if (theWindow->mouseFirstMoved)
+	{
+		theWindow->lastX = xPos;
+		theWindow->lastY = yPos;
+		theWindow->mouseFirstMoved = false;
+	}
+
+	// Fix 2: Use += instead of = to accumulate all movements in the frame
+	theWindow->xChange += xPos - theWindow->lastX;
+	theWindow->yChange += yPos - theWindow->lastY; // Reversed Y is correct for camera pitch
+
+	theWindow->lastX = xPos;
+	theWindow->lastY = yPos;
+}
+
+GLWindow::~GLWindow()
+{
+	glfwDestroyWindow(mainWindow);
+	glfwTerminate();
+}
